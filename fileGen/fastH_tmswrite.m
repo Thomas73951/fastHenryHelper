409b58e846517e53491813fdc5c6bc055015ac73
Thomas Sharratt <thomassharratt@hotmail.co.uk>
2023-12-29 17:07:47 +0000

    filegen-inp files into indiv. subfolders

--- fileGen/fastH_tmswrite.m
+++ fileGen/fastH_tmswrite.m
@@ -17,7 +17,8 @@ clc
 
 
 WRITE_FOLDER = 'testfiles/'; % file name is auto generated
-SAVE_IMG = false;
+SAVE_IMG = false; % save figures in images folder
+USE_SUBFOLDERS = false; % puts each file into a subfolder
 
 % units in mm.
 s = [0.4 0.1]; % spacing
@@ -45,7 +46,16 @@ for iterINP = 1:size(offset2, 1)
   fileName = ['fh_C1_T', num2str(turns(1)), '_ID', num2str(id(1)), '_S', num2str(s(1)), ...
               '_C2_T', num2str(turns(2)), '_ID', num2str(id(2)), '_S', num2str(s(2)), ...
               '_O' num2str(offset2(iterINP,1)), '.', num2str(offset2(iterINP,2)), '.inp']
-  file = fopen([WRITE_FOLDER, fileName],'wt');
+  if (USE_SUBFOLDERS)
+    subfolder = [num2str(iterINP), '_Offset_', ...
+                 num2str(offset2(iterINP,1)), ',', num2str(offset2(iterINP,2))];
+    if (!exist([WRITE_FOLDER, subfolder, '/']))
+      mkdir(WRITE_FOLDER, subfolder);
+    endif
+    file = fopen([WRITE_FOLDER, subfolder, '/', fileName],'wt');
+  else
+    file = fopen([WRITE_FOLDER, fileName],'wt');
+  endif
 
   fprintf(file, horzcat('* Fasthenry file "', fileName, '" generated from fastH_tmswrite.m\n'));
   fprintf(file, '.units mm\n');
@@ -84,8 +94,8 @@ for iterINP = 1:size(offset2, 1)
   ylim([(min(y2) - 0.1), (max(y2) + 0.1)])
   legend('FontSize',11)
   title(['Coil 2 (z = ', num2str(z(2)), ', Offset=(', ...
-        num2str(offset2(iterINP,1)), ',', num2str(offset2(iterINP,2)), ...
-        ')) [dimensions in mm]'])
+         num2str(offset2(iterINP,1)), ',', num2str(offset2(iterINP,2)), ...
+         ')) [dimensions in mm]'])
   % turn set of x,y values into fasthenry nodes
   N2Max = N1Max + size(x2,2);
   file = nodePrint(file, N2Min, N2Max, x2, y2, z(2));
