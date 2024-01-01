64138a407e57c01ea5628f6a3ecd0f7ef828daf0
Thomas Sharratt <thomassharratt@hotmail.co.uk>
2024-01-01 20:45:34 +0000

    saveImages- fixed multiple date issue
    caused wrong figure order in file explorer
    datetime is now same for all saved

--- fileGen/saveImages.m
+++ fileGen/saveImages.m
@@ -7,11 +7,12 @@ function saveImages()
   endif
 
   figHandles = get(groot, 'Children');
+  currTime = datestr(clock, 'yy-mm-dd HH-MM-SS');
   for i = 1:size(figHandles,1)
-    imfile = ['images/',datestr(clock, 'yy-mm-dd HH-MM-SS'),' Fig', num2str(figHandles(i,:)), '.png'];
+    imfile = ['images/', currTime,' Fig', num2str(figHandles(i,:)), '.png'];
     saveas(figHandles(i,:),imfile)
   endfor
 endfunction
 
-% function file saveImages created by Thomas Sharratt Copyright (C) 2023
+% function file saveImages created by Thomas Sharratt Copyright (C) 2024
 % <Created for Octave on Arch Linux>
