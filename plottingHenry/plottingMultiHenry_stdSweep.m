9b798a4d912e57d75a88ec56784d68c3a51d856c
Thomas Sharratt <thomassharratt@hotmail.co.uk>
2024-01-09 17:58:29 +0000

    plotting- of current standard set...
    all of same type on same fig (Sweeps2-5 (all x)) all on same plot
    should work for any set of sweeps tbh - needs evaluation and tidy up

---
+++ plottingHenry/plottingMultiHenry_stdSweep.m
@@ -0,0 +1,130 @@
+clear all
+close all
+clc
+
+% STANDARD SWEEP VERSION. SWEEP1 = Z SWEEP, SWEEP2-5 = X SWEEPS AT DIFFERENT Z
+
+% <Created for Octave on Arch Linux & Windows>
+% Plotting for fastHenry results in csv file created by fasthenryhelper/automatedhenry/
+% Requires a sweep over an offset direction.
+%
+% Uses function file: saveImages.m
+%
+% plottingHenry.m created by Thomas Sharratt Copyright (C) 2024
+
+
+%% USER DEFINED >
+% Read folder is top folder containing sweep folders.
+READ_FOLDER = ['results', filesep]; %, 'offset-y-0-20-101', filesep];
+FILE_NAME_PART_INDUCTANCES = "offset-test5";
+SAVE_IMG = true; % save figures in images folder
+PLOT_MARKER = '-'; % global plot marker for this script
+% < END OF user defined
+
+function sweepType = determineSweepType(sweepX, sweepY, sweepZ)
+  % determine sweep type
+  sweepType = [0, 0, 0];
+  if (!all(sweepX(:) == sweepX(1))) % not all same value => sweep in x
+    sweepType(1,1) = 1;
+  endif
+
+  if (!all(sweepY(:) == sweepY(1))) % not all same value => sweep in y
+    sweepType(1,2) = 1;
+  endif
+
+  if (!all(sweepZ(:) == sweepZ(1))) % not all same value => sweep in y
+    sweepType(1,3) = 1;
+  endif
+endfunction
+
+for i = 1:5
+  %% Read csv file
+  csvFileName = [FILE_NAME_PART_INDUCTANCES, '_Sweep', num2str(i), '_inductances.csv'];
+  data = csvread([READ_FOLDER, csvFileName]);
+  sweepX = data(:,2); % requires comma separated folder name: ".../Offset,x,y/"
+  sweepY = data(:,3);
+  sweepZ = data(:,4);
+  L1 = data(:,6);
+  L2 = data(:,7);
+  M = data(:,8);
+
+  % calculate coupling factor (k)
+  k = M ./ sqrt(L1.*L2);
+
+  sweepType = determineSweepType(sweepX, sweepY, sweepZ);
+
+  if (sum(sweepType) > 1)
+    error("2D & 3D SWEEP, currently unsuported")
+    % 2d sweep, unsupported rn.
+  elseif (sweepType == [1,0,0]) % x sweep
+    sweepVar = sweepX;
+    sweepAxis = "x";
+    constCoords = ['y = ', num2str(sweepY(1)), ', z = ', num2str(sweepZ(1))];
+    figNumStart = 1; % TODO: bodge fix of current problems
+  elseif (sweepType == [0,1,0]) % y sweep
+    sweepVar = sweepY;
+    sweepAxis = "y";
+    constCoords = ['x = ', num2str(sweepX(1)), ', z = ', num2str(sweepZ(1))];
+    figNumStart = 5;
+  elseif (sweepType == [0,0,1]) % z sweep
+    sweepVar = sweepZ;
+    sweepAxis = "z";
+    constCoords = ['x = ', num2str(sweepX(1)), ', y = ', num2str(sweepY(1))];
+    figNumStart = 9;
+  else
+    error("NO SWEEP FOUND. Cannot plot over single point")
+  endif
+
+
+  %% Plotting
+  % L1
+  figure(figNumStart)
+  grid on
+  hold on
+  plot(sweepVar, L1, PLOT_MARKER, 'DisplayName', ['L1 ', constCoords])
+  legend('FontSize',11)
+  % xlim([sweepVar(1), sweepVar(end)])
+  xlabel(["Sweep over ", sweepAxis, "-axis [mm]"])
+  ylabel("Inductance [H]")
+  title(['L1 (reader) Swept Over ', sweepAxis, '-axis'])
+
+  % L2
+  figure(figNumStart + 1)
+  grid on
+  hold on
+  plot(sweepVar, L2, PLOT_MARKER, 'DisplayName', ['L2 ', constCoords])
+  legend('FontSize',11)
+  % xlim([sweepVar(1), sweepVar(end)])
+  xlabel(["Sweep over ", sweepAxis, "-axis [mm]"])
+  ylabel("Inductance [H]")
+  title(['L2 (tag) Swept Over ', sweepAxis, '-axis'])
+
+
+  % M
+  figure(figNumStart + 2)
+  grid on
+  hold on
+  plot(sweepVar, M, PLOT_MARKER, 'DisplayName', ['M ', constCoords])
+  legend('FontSize',11)
+  % xlim([sweepVar(1), sweepVar(end)])
+  xlabel(["Sweep over ", sweepAxis, "-axis [mm]"])
+  ylabel("Inductance [H]")
+  title(['Mutual Inductance Swept Over ', sweepAxis, '-axis'])
+
+
+  % K
+  figure(figNumStart + 3)
+  grid on
+  hold on
+  plot(sweepVar, k, PLOT_MARKER, 'DisplayName', ['k ', constCoords])
+  legend('FontSize',11)
+  % xlim([sweepVar(1), sweepVar(end)])
+  xlabel(["Sweep over ", sweepAxis, "-axis [mm]"])
+  ylabel("Coupling Factor []")
+  title(['Coupling Factor (k) Swept Over ', sweepAxis, '-axis'])
+endfor
+
+%% save figures?
+if (SAVE_IMG)
+  saveImages();
+endif
